aa9adc7c8fe61b8aac2f3de2dd17f5c81ccfa793
Soussi Haythem <soussih@gmail.com>
2020-04-16 20:21:57 +0100

    Add entité 2

---
+++ src/main/java/org/jhipster/blogtuto/repository/TagRepository.java
@@ -0,0 +1,14 @@
+package org.jhipster.blogtuto.repository;
+
+import org.jhipster.blogtuto.domain.Tag;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Tag entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
